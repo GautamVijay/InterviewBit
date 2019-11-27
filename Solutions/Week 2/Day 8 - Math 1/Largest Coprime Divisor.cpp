fb5b36093b4e76960e1fd72533b1e9c2203ed73d
GautamVijay <kgpvijaybg@gmail.com>
2019-11-28 00:51:07 +0530

    commit

--- Solutions/Week 2/Day 8 - Math 1/Largest Coprime Divisor.cpp
+++ Solutions/Week 2/Day 8 - Math 1/Largest Coprime Divisor.cpp
@@ -1,6 +1,5 @@
-/*
+/* 
 Largest Coprime Divisor
-------------------------
 You are given two positive numbers A and B. You need to find the maximum valued integer X such that:
 X divides A i.e. A % X = 0
 X and B are co-prime i.e. gcd(X, B) = 1
@@ -9,10 +8,9 @@ A = 30
 B = 12
 We return
 X = 5
+ */
 
-*/
 
-// GCD find utility
 int gcd (int x, int y){
     if (x == y)
         return x;
